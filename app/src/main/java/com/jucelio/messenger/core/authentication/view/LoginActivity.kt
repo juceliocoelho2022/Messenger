d615c6e830a867f43aa6823e878fdf94d8afb2e9
Jucelio coelho <juceliocoelho2022@gmail.com>
2024-03-05 16:32:06 -0300

    Fragment ok

---
+++ app/src/main/java/com/jucelio/messenger/core/authentication/view/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.jucelio.messenger.core.authentication.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.jucelio.messenger.R
+
+interface LoginCallback{
+    fun onCreateAccountClicked()
+}
+class LoginActivity : AppCompatActivity(),LoginCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+    }
+
+    override fun onCreateAccountClicked() {
+        val fragment = SignUpFragment()
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view, fragment)
+            addToBackStack(null)
+        }
+    }
+}
